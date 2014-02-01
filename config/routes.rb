76d1f4010b8b49ad37181f3c77cfabf2e31959c2
Kurnia Muhamad <omenkzz@gmail.com>
2014-02-01 23:57:45 +0700

    add login feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Hytest::Application.routes.draw do
+
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
   resources :users
+  resources :sessions
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
